60e12b9a94e54566efab7537554c5f72869d1e26
zsxneil <zsxneil@hotmail.com>
2017-12-13 17:42:11 +0800

    实体类

---
+++ springdataelasticsearch/src/main/java/com/my/model/Article.java
@@ -0,0 +1,57 @@
+package com.my.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/12/13.
+ */
+@Document(indexName = "article_index",type = "article_type")
+@Setting(settingPath = "article_analyzer.json")
+public class Article implements Serializable {
+    @Id
+    private Integer id;
+    @Field(type = FieldType.text,analyzer = "ik_max_word")
+    private String title;
+    @Field(type = FieldType.text,analyzer = "ik_max_word")
+    private String content;
+    @Field(type = FieldType.Date,store = true,format = DateFormat.custom,pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+    private String releaseDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
